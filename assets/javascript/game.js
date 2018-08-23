4d6d7c6a0a366e1be7d475b4bf264107d5e49df3
Andy <28871636+aschlegel09@users.noreply.github.com>
2018-08-23 16:20:40 -0400

    small js updates
    Need to adjust div id's in html as well as <p> tags for Return value of Attack button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,67 +2,73 @@
 $(document).ready(function() {
   // Provide the initial players as objects with predefined hp and power
   // Be sure to adjust base power once first click occurs
-  var characters = [dany, jonSnow, cersei, nightKing];
-  var dany = [
-    (name = "Danaerys Targaryen"),
-    (attackPower = 120),
-    (basePower = 20),
-    (health = 120),
-    $(".dany")
-  ];
-  var jonSnow = [
-    (name = "Jon Snow"),
-    (attackPower = 120),
-    (basePower = 20),
-    (health = 120),
-    $(".jon")
-  ];
-  var cersei = [
-    (name = "Cersei Lannister"),
-    (attackPower = 120),
-    (basePower = 20),
-    (health = 120),
-    $(".cersei")
-  ];
-  var nightKing = [
-    (name = "Night King"),
-    (attackPower = 120),
-    (basePower = 20),
-    (health = 120),
-    $(".king")
-  ];
-
-  // var playerBtn = $("<div>");
-  // Create an on click event attached to the player class
-  $(".player").on("click", function() {
-    for (var i = 0; i < characters.length; i++) {
-
-
-
-
-    }
+  var characters = ["Dany", "Jon Snow", "Cersei", "Night King"];
+  console.log("My fav Game of Thrones characters are: " + characters);
+	
+	var dany = {
+    name : "Danaerys Targaryen",
+    attackPower : 120,
+    basePower : 20,
+    health : 120 + " hp",
+//    $(".dany")
+  };
+  var jonSnow = {
+    name : "Jon Snow",
+    attackPower : 120,
+    basePower : 20,
+    health : 120 + " hp",
+//    $(".jon")
+  };
+  var cersei = {
+    name : "Cersei Lannister",
+    attackPower : 120,
+    basePower : 20,
+    health : 120 + " hp",
+//    $(".cersei")
+  };
+  var nightKing = {
+    name : "Night King",
+    attackPower : 120,
+    basePower : 20,
+    health : 120 + " hp",
+//    $(".king")
+  };
+	console.log(dany.name);
+	console.log("Jon Snow's health is " + jonSnow.health);
+	console.log("Cersei Lannister's attack power is " + cersei.attackPower);
+	console.log("The Night King's base power is " + nightKing.basePower);
 
 
-
-    
-    var fighterOne = $("#fighter").length; // the first clicked player is equal to the first section div
+	
+ var fighterOne = $("#fighter").length; // the first clicked player is equal to the first section div
     var fighterTwo = $("#enemies").length;
     var fighterThree = $("#defenders").length;
     var playerMagnet = $(".player");
-    var jon = $(".jon");
-    var dany = $(".dany");
-    var king = $(".king");
-    var cersei = $(".cersei");
+    var jonDiv = $(".jon");
+    var danyDiv = $(".dany");
+    var kingDiv = $(".king");
+    var cerseiDiv = $(".cersei");
+	
+	
+	
+	
+  // var playerBtn = $("<div>");
+  // Create an on click event attached to the player class
+  $("#danyId").on("click", function() {
+	  // goes thru characters array of objects, and should find each one is linked to its specific character div
+    for (var i = 0; i < danyDiv.length; i++) {
+	//for each object in array...	
+		console.log("My character is " + characters[i]);
 
     if (fighterOne >= 1) {
-      $("#fighter").append($(dany));
+      $("#fighter").append($(danyDiv));
     }
     if (fighterTwo >= 1) {
-      $("#enemies").append($(jon));
+      $("#enemies").append($(jonDiv));
     }
     if (fighterThree >= 1) {
-      $("#defenders").append($(king));
-      $("#defenders").append($(cersei));
+      $("#defenders").append($(kingDiv));
+      $("#defenders").append($(cerseiDiv));
     }
     // Create a variable playerMagnet and set equal to a new div
 
@@ -72,7 +78,7 @@ $(document).ready(function() {
     // playerMagnet.text($(this).attr("data-letter"));
     // append var to result div
     // $("#fighter").append(playerMagnet);
-  });
+  }});
   // Move Player to Fighter position on first click
   //   $(".player").on("click", function() {
   //     $("#").appendTo(player);
@@ -81,7 +87,14 @@ $(document).ready(function() {
   // Move first clicked player to Fighter Row
   // Remaining Players move to Enemies Row
   // Click on an Enemy player and move them to the Defender section
-  // Press Attack Button to begin Combat
+   $(".attack").on("click", function() {
+	  // Press Attack Button to begin Combat
+	  //Event Handler: Return
+	   console.log(event.target.textContent + event.detail);
+	  $("p").text("Combat has ensued!");//need to adjust p tags in html
+	   
+	   
+	  });
   // Defender hiddem when HP = 0
   // When there is no player to attack, Remind them
   // Set Players as Objects
